11d3a9e5cc264a0a83829dcbfbc544500015b193
LizzieDworsky <amydworsky15@gmail.com>
2023-03-16 17:21:19 -0600

    simple excersises to get used to C# syntax

---
+++ C#Intro/VariablesLab/VariablesLab/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace VariablesLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 32;
+            Console.WriteLine($"I am {myAge} years old");
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+            string fullName = firstName + " " + lastName;
+        }
+    }
+}
