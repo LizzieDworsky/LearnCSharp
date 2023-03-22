028b15edf70f41ac50baa14c39eddb4b73c0e594
LizzieDworsky <amydworsky15@gmail.com>
2023-03-22 16:45:21 -0600

    added fizzbuzz

--- C#Intro/LoopsLab/LoopsLab/Program.cs
+++ C#Intro/LoopsLab/LoopsLab/Program.cs
@@ -6,38 +6,59 @@
         {
             Console.WriteLine("Loops");
 
-            // Task 1 - Five Hellos
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Hello");
-            }
+            //// Task 1 - Five Hellos
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Hello");
+            //}
 
-            // Task 2 - Count
-            for (int i = 0; i < 11; i++)
-            {
-                Console.WriteLine(i);
-            }
+            //// Task 2 - Count
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
-            // Task 3 - Count Backwards
-            for (int i = 10; i >= 0; i--)
-            {
-                Console.WriteLine(i);
-            }
+            //// Task 3 - Count Backwards
+            //for (int i = 10; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
-            // Task 4 - Prompt Output
-            Console.WriteLine("How many times would you like to print devCodeCamp?");
-            int userInput = int.Parse(Console.ReadLine());
-            for (int i = 0; i < userInput; i++)
-            {
-                Console.WriteLine("devCodeCamp");
-            }
+            //// Task 4 - Prompt Output
+            //Console.WriteLine("How many times would you like to print devCodeCamp?");
+            //int userInput = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < userInput; i++)
+            //{
+            //    Console.WriteLine("devCodeCamp");
+            //}
 
-            // Task 5 - String out a String
-            Console.WriteLine("Enter a string");
-            string str = Console.ReadLine();
-            foreach (char letter in str)
+            //// Task 5 - String out a String
+            //Console.WriteLine("Enter a string");
+            //string str = Console.ReadLine();
+            //foreach (char letter in str)
+            //{
+            //    Console.WriteLine(letter);
+            //}
+
+            // Task 6 - Fizz Buzz
+            for (int i = 0; i < 101; i++)
             {
-                Console.WriteLine(letter);
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                } 
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
 
 
