9ccea2088f5f20ac622a130bc9ba4a543e13a210
LizzieDworsky <amydworsky15@gmail.com>
2023-03-22 16:37:33 -0600

    working on for loops lab, WIP

--- C#Intro/LoopsLab/LoopsLab/Program.cs
+++ C#Intro/LoopsLab/LoopsLab/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Lab");
+            Console.WriteLine("Loops");
+
+            // Task 1 - Five Hellos
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello");
+            }
+
+            // Task 2 - Count
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Task 3 - Count Backwards
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Task 4 - Prompt Output
+            Console.WriteLine("How many times would you like to print devCodeCamp?");
+            int userInput = int.Parse(Console.ReadLine());
+            for (int i = 0; i < userInput; i++)
+            {
+                Console.WriteLine("devCodeCamp");
+            }
+
+            // Task 5 - String out a String
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            foreach (char letter in str)
+            {
+                Console.WriteLine(letter);
+            }
+
+
+
         }
     }
 }
