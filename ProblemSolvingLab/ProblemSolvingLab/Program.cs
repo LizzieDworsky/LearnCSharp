18d1453de779dc8019c4d43d6b4f59f2ed3e09f7
LizzieDworsky <amydworsky15@gmail.com>
2023-03-29 19:42:20 -0600

    WIP Problem solving sheet, added algrorithms for 4 tasks + completed the first task

---
+++ ProblemSolvingLab/ProblemSolvingLab/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ProblemSolvingLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Task 1: Reverse a string
+            //    Algorithm:
+            //    1. need to get user input, a string to reverse
+            //    2. need the length of the string
+            //    3. create a variable that will be the reversed string
+            //    4. use the length to get the starting place
+            //    5. step backwards from the end and add to the reversed string variable
+
+            Console.WriteLine("Please enter a string for reversal:");
+            string userInput = Console.ReadLine();
+            string reversedString = "";
+            for(int i = userInput.Length - 1; i > -1; i--)
+            {
+                reversedString += userInput[i];
+            }
+            Console.WriteLine(reversedString);
+
+
+            //Task 2: Capitalize Last Letter
+            //    Algorithm:
+            //    1. need to get user input, a string to capitalize
+            //    2. need the length of the string
+            //    3. iterate through the string
+            //    4. use the length to capitalize the final index letter
+            //    5. use any spaces to capitalize the last letter of the words
+
+
+
+
+
+
+
+            //Task 3: Palindrome
+            //    Algorithm:
+            //    1. need user input, a string to check
+            //    2. going to use the previous reverse string code to get a reversed version of user input
+            //    3. compare the two strings, original and reversed
+            //    4. if equal, palindrome ✅ otherwise ❎
+
+
+
+
+
+
+
+
+            //Task 4: Compress a string of characters
+            //    Algorithm:
+            //    1. create string for compression
+            //    2. need the length of the string
+            //    3. use the length to iterate through the string
+            //    4. create an empty string to capture the compressed string
+            //    5. create a counter
+            //    6. capture the first character of the string (at index 0) in a variable
+            //    7. compare the characters, starting with the first one
+            //    8. if they match add to counter
+            //    9. if they don't reset counter and change variable for comparison to that character
+            //    after adding the counter and current character to the compression string
+
+
+        }
+    }
+}
